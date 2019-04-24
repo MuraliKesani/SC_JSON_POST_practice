df78a58a3f93bcee6b952d1d34d83bb979820d27
Murali <kesani.murali@achiever-68.local>
2019-04-24 10:27:15 +0530

    Initial Commit

---
+++ SC_JSON_POST_practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SC_JSON_POST_practice
+//
+//  Created by Murali on 4/24/19.
+//  Copyright © 2019 Murali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
