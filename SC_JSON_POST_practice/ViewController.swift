354b07cc3b632116fa8bbba4d0755375277f7bb3
Murali <kesani.murali@achiever-68.local>
2019-04-24 11:26:02 +0530

    Created URLRequest,httpMethod,httpBody

--- SC_JSON_POST_practice/ViewController.swift
+++ SC_JSON_POST_practice/ViewController.swift
@@ -10,12 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var loginBTN: UIButton!
+    
+    var urlREQ:URLRequest!
+    var sessionDataTask:URLSessionDataTask!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //JSON_POST practice
+        
+        emailTF.text = "kesani.murali@gmail.com"
+        passwordTF.text = "munna341820"
+        
     }
 
-
+    @IBAction func onLoginBTNTap(_ sender: UIButton)
+    {
+        
+        urlREQ = URLRequest(url: URL(string: "https://www.brninfotech.com/pulse/modules/admin/ValidateLogin.php")!)
+        urlREQ.httpMethod = "POST"
+        
+        let dataToSend = "registeredEmail=\(self.emailTF.text!)&registeredPassword=\(self.passwordTF.text!)&funcName=verifyLogin"
+        
+        urlREQ.httpBody = dataToSend.data(using: String.Encoding.utf8)
+        
+        sessionDataTask = URLSession.shared.dataTask(with: urlREQ, completionHandler: { (data, response, err) in
+            
+            print(data!)
+            print("Got Response")
+          
+            
+        })
+        sessionDataTask.resume()
+        
+    }
+    
+    
+    
 }
 
